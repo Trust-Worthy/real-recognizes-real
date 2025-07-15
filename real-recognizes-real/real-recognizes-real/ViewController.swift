6d86a2a5808643703549fa0669b28f6f704a1bae
Trust-Worthy <jondeveloper0@gmail.com>
2025-07-15 13:05:42 -0600

    Restarting because crap wasn't working.

--- real-recognizes-real/real-recognizes-real/ViewController.swift
+++ real-recognizes-real/real-recognizes-real/ViewController.swift
@@ -2,62 +2,38 @@
 //  ViewController.swift
 //  real-recognizes-real
 //
-//  Created by Trust-Worthy on 7/15/25.
+//  Created by Jonathan Bateman on 7/15/25.
 //
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource, UISearchBarDelegate{
+class ViewController: UIViewController, UITableViewDataSource {
     
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        
+        cell.textLabel?.text = "Row \(indexPath.row)"
+        
+        return cell
+    }
+    
+
     @IBOutlet weak var tableView: UITableView!
     
-    var accounts: [Account] = []
-    var filteredAccounts: [Account] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         tableView.dataSource = self
-        
-        
-        self.accounts = [ Account(name: "Alice @ Gmail", otp: "123456"), Account(name: "Bob @ Work", otp: "789012")]
-        filteredAccounts = accounts
-    }
-    
-    // MARK: tableView functions
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return filteredAccounts.count
-        
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "AccountCell", for: indexPath) as! AccountCell
-        
-        let account = accounts[indexPath.row]
-        
-        cell.textLabel?.text = account.name
-        cell.detailTextLabel?.text = account.otp
-        
-        return cell
-        
-    
-    }
-    
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        
-        
-        filteredAccounts = searchText.isEmpty ? accounts : accounts.filter {
-            $0.name.lowercased().contains(searchText.lowercased())
-        }
-        
-        // reload the tableview after any changes
-        tableView.reloadData()
     }
 
-    
+
 }
 
-	
