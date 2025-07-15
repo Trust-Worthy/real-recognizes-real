32e68148f564c13808df3f63306846ea8c1643a4
Trust-Worthy <jondeveloper0@gmail.com>
2025-07-15 11:01:42 -0600

    This is where the fun begins.

---
+++ real-recognizes-real/real-recognizes-real/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  real-recognizes-real
+//
+//  Created by Jonathan Bateman on 7/15/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = UIColor
+    }
+
+
+}
+
