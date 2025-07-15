0897a091fced7a283bf008ca1d218cd995c9e340
Trust-Worthy <jondeveloper0@gmail.com>
2025-07-15 13:22:02 -0600

    Adding the account cell class.

---
+++ real-recognizes-real/real-recognizes-real/AccountCell.swift
@@ -0,0 +1,23 @@
+//
+//  AccountCell.swift
+//  real-recognizes-real
+//
+//  Created by Jonathan Bateman on 7/15/25.
+//
+
+import UIKit
+
+class AccountCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
